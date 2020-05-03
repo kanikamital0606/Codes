d0a6818e0f33d67c7ddd6a33ebee35097525fe2d
Kanika Mittal <kanikamittal0661996@gmail.com>
2020-05-04 04:12:27 +0530

    update

---
+++ 5.Recursion/Recursion Path/floodFill.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+    }
+
+    public static void floodfill(int[][] maze, int row, int col, String psf, boolean[][] visited){
+        
+    }
+}
--- 5.Recursion/Recursion_Array/d_firstIndex.java
+++ 5.Recursion/Recursion_Array/d_firstIndex.java
@@ -1,15 +1,40 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class d_firstIndex {
 
     public static void main(String[] args) throws Exception {
         // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr =  new int[n];
+
+        for(int i=0; i<arr.length; i++){
+        	arr[i] = scn.nextInt();
+        }
+
+        int x = scn.nextInt();
+
+        int fi = firstIndex(arr,0,x);
+        System.out.println(fi);
     }
 
     public static int firstIndex(int[] arr, int idx, int x){
-        return 0;
+
+    	//base case
+    	if(arr[idx] == arr.length){
+    		return -1;
+    	}
+
+    	if(arr[idx] ==x){
+    		return idx;
+    	}
+
+    	else{
+    		int firstIdx = firstIndex(arr,idx+1,x);
+
+    		return firstIdx;
+    	}
     }
 
 }
-d_firstIndex.java
