4a1ce37e5ea7bf33a31b2205605e92a1b41e637d
Kanika Mittal <kanikamittal0661996@gmail.com>
2020-05-04 11:47:50 +0530

    update

--- 5.Recursion/Recurion_String/getSubsequence.java
+++ 5.Recursion/Recurion_String/getSubsequence.java
@@ -1,3 +1,9 @@
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Get Subsequence
+// 1. You are given a string str.
+// 2. Complete the body of getSS function - without changing signature - to calculate all subsequences of str.
+// Use sample input and output to take idea about subsequences.
+//												SAMPLE INPUT: 		abc
+// 												SAMPLE OUTPUT:		[, c, b, bc, a, ac, ab, abc]
 import java.io.*;
 import java.util.*;
 
@@ -8,15 +14,18 @@ public class getSubsequence{
 		String str = scn.nextLine();
 		ArrayList<String> ans = gss(str);
 		System.out.println(ans);
-	}
 
+		 
+		
+		
+	}
 	public static ArrayList<String> gss(String str){
+		
 		if(str.length()==0){
 			ArrayList<String>baseAns = new ArrayList<>();
 			baseAns.add("");
 			return baseAns;
 		}
-
 		ArrayList<String> recAns = gss(str.substring(1));
 		char ch = str.charAt(0);
 
@@ -25,7 +34,6 @@ public class getSubsequence{
 		for(int i=0; i<recAns.size(); i++){
 			myAns.add(recAns.get(i));
 		}
-
 		for(int i=0; i<recAns.size(); i++){
 			myAns.add(ch + recAns.get(i));
 		}
