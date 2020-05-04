1a0ed965484d7db747b8c92439e1287263a07c1e
Kanika Mittal <kanikamittal0661996@gmail.com>
2020-05-04 05:41:51 +0530

    update

--- 5.Recursion/Recursion_Array/d_firstIndex.java
+++ 5.Recursion/Recursion_Array/d_firstIndex.java
@@ -1,10 +1,8 @@
 import java.io.*;
 import java.util.*;
-
 public class d_firstIndex {
-
-    public static void main(String[] args) throws Exception {
-        // write your code here
+	public static void main(String[] args) throws Exception {
+        
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         int[] arr =  new int[n];
@@ -12,29 +10,23 @@ public class d_firstIndex {
         for(int i=0; i<arr.length; i++){
         	arr[i] = scn.nextInt();
         }
-
+        //data to be search 
         int x = scn.nextInt();
 
         int fi = firstIndex(arr,0,x);
         System.out.println(fi);
     }
-
     public static int firstIndex(int[] arr, int idx, int x){
-
     	//base case
-    	if(arr[idx] == arr.length){
+    	if(idx == arr.length){
     		return -1;
     	}
-
     	if(arr[idx] ==x){
     		return idx;
     	}
-
     	else{
     		int firstIdx = firstIndex(arr,idx+1,x);
-
     		return firstIdx;
     	}
     }
-
 }
