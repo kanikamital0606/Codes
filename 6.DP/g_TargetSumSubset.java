cebfed22b76fc607c7727a46789c96d1e7eb0a1e
Kanika Mittal <kanikamittal0661996@gmail.com>
2020-05-07 10:47:40 +0530

    update

---
+++ 6.DP/g_TargetSumSubset.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class g_TargetSumSubset{
+
+	public static void main(String[] args) throws Exception {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int[] arr = new int[n];
+
+		for(int i=0; i<arr.length; i++){
+			arr[i] = scn.nextInt();	
+		}
+
+		int tar = scn.nextInt();
+		System.out.println(func(arr, tar));
+	}
+	public static boolean func(int[] arr, int sum) {
+		int n = arr.length;
+
+		boolean dp[][] = new boolean[n + 1][sum + 1];
+
+		for (int i = 0; i < dp.length; i++) {
+
+			for (int j = 0; j < dp[0].length; j++) {
+
+				if (j == 0) {
+
+					dp[i][j] = true;
+				} else if (i >= 1 && dp[i - 1][j]) {
+
+					dp[i][j] = dp[i - 1][j];
+				} else if (i >= 1 && j - arr[i - 1] >= 0) {
+
+					dp[i][j] = dp[i - 1][j - arr[i - 1]];
+				}
+			}
+		}
+		return dp[arr.length][sum];
+	}
+	public static int findTargetSumWays(int[] nums, int S) {
+		int sum = 0;
+		for (int num : nums)
+			sum += num;
+		if (sum < S || (sum + S) % 2 == 1)
+			return 0;
+		int s = (sum + S) / 2;
+		int[] dp = new int[s + 1];
+		dp[0] = 1;
+		for (int n : nums)
+			for (int i = s; i >= n; i--)
+				dp[i] += dp[i - n];
+		return dp[s];
+	}
+}
